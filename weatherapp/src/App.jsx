ca8999f727b3ced76ecf06687a2fee26eb3f6d30
Pinkee Maurya <akpipinkymaurya@gmail.com>
2024-03-07 16:02:06 +0530

    file is added .

---
+++ weatherapp/src/App.jsx
@@ -0,0 +1,91 @@
+import React, { useState, useEffect } from 'react';
+// import './App.css';
+import Temperature from './component/Temperature';
+import Highlights from './component/Highlights';
+
+function App() {
+  const [city, setCity] = useState('New Delhi');
+  const [weatherData, setWeatherData] = useState(null);
+
+  const apiURL = `http://api.weatherapi.com/v1/current.json?key=a571705074944117bda61602240703&q=${city}&aqi=no`;
+
+  useEffect(() => {
+    
+      
+    fetch(apiURL)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Error");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setWeatherData(data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+      
+  }, [city]);
+
+  return (
+    <div className='bg-[hsl(230,53%,7%)] h-screen flex justify-center align-top'>
+      <div className='mt-40 w-1/5 h-1/3'>
+        {weatherData && (
+        
+        <Temperature setCity={setCity} 
+        stats={{
+          
+            temp: weatherData.current.temp_c,
+            condition: weatherData.current.condition.text,
+            isDay: weatherData.current.is_day,
+            location: weatherData.location.name,
+            time: weatherData.location.localtime,
+          }}
+
+        />
+        )}
+      </div>
+      <div className='mt-40 w-1/3 h-1/3 p-10 grid grid-col-2 gap-6'>
+        <h2 className='text-slate-200 text-2xl col-span-2'>
+           Today's Highlights</h2>
+        {weatherData && (
+          <>
+            <Highlights
+              stats={{
+                title: "Wind Status",
+                value: weatherData.current.wind_mph,
+                unit: "mph",
+                direction: weatherData.current.wind_dir,
+              }}
+            />
+            <Highlights
+              stats={{
+                title: "Humidity",
+                value: weatherData.current.humidity,
+                unit: "%",
+              }}
+            />
+            <Highlights
+              stats={{
+                title: "Visibility",
+                value: weatherData.current.vis_miles,
+                unit: "miles",
+              }}
+            />
+            <Highlights
+              stats={{
+                title: "Air Pressure",
+                value: weatherData.current.pressure_mb,
+                unit: "mb",
+              }}
+            />
+          </>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ weatherapp/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  
+    content: [
+      "./index.html",
+      "./src/**/*.{js,js,jsx,tsx}",
+    ],
+  
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
